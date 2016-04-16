b0e9c09d7abdf13829b7998e491803ee82266e0d
macv101 <michaelvillar0012@gmail.com>
2016-04-16 15:00:09 -0700

    Testing with PHP

---
+++ phpTest.php
@@ -0,0 +1,28 @@
+	
+	<?php
+	$page_title = "About page";
+	include('includes/header.html');
+	?>
+	
+	<div class="about" id="about2">
+		<div class="container">
+	
+
+	   <div class="row">
+      <!-- YOUTUBE VIDEO RESPONSIVE-->
+      <div class=" col-lg-6 block-center">
+          <div class="flex-video widescreen" style="margin: 0 auto;text-align:center;">
+              <iframe width="200" height="200"allowfullscreen="" src="https://www.youtube.com/embed/VNZTq9vq5kk" frameborder="0">
+              </iframe>
+          </div>
+      </div>
+  </div>
+
+			<p>The JesusWalk Youth Conference is designed to open the eyes of our youth to the potential they have in Christ. Our vision is that every student breaks from their worldly chains to become who God has designed them to be—each in their own way a disciple furthering His Kingdom. Our mission at the JesusWalk Youth Conference is to provide an open and safe refuge for youth to experience God and His Word. We accomplish this through a four-day dynamic program that includes moving worship, engaging messages, community service, and extensive mentoring by small group interaction. 1 John 2:6 says, “Whoever claims to live in him must live as Jesus did.” We give our students a clear picture of how to move beyond the conference—influenced and driven by the Gospel to continually live in His example. What drives JesusWalk is the people who help make it happen. We’ve built this conference with our own youth &amp; young adults to work together as one body and strive for one goal: transform the lives of our students. With the guidance of the CCMP, JesusWalk continues to grow and find new ways of reaching out.</p>
+		</div>	
+	</div>
+	
+	<?php
+	include('includes/footer.html');
+	?>
+	
