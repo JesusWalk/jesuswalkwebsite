0fa4db83e673d7fde76457d45ed10d5c35b5e619
macv101 <michaelvillar0012@gmail.com>
2016-04-16 16:01:23 -0700

    Added PHP functionality. Created include folder with header.html and footer.html. Still needs to undergo testing.

---
+++ 2016.php
@@ -0,0 +1,93 @@
+<?php
+	$page_title = "Theme";
+	$page_banner="themeBanner";
+	$page_header="";
+	include('includes/header.html');
+	?>
+
+<!--about-->
+
+
+	<div class="about" id="theme">
+		<div class="container">
+			<p>Our theme this year is "This Is It”. The culture we live in today teaches us that we have to earn
+			everything. We constantly have to prove our worth and value as individuals and must strive to be
+			someone and not just a face in the crowd. This summer, we will be diving into what it means to be the
+			Church. As John 3:16 says, “For God so loved the WORLD, that He gave His only Son…” We quickly forget
+			that Jesus did not just come to save the righteous but everyone in this world no matter who you are. And
+			as followers of Jesus Christ, we are not alone but rather have been given the opportunity to be a part of
+			a world changing force called the Church.
+			This year at JesusWalk, we will be dedicating each day to going in depth on what this means:
+			</p>
+
+			<h2>This is it: We are Welcomed</h2>
+			<p>Gospel presentation night: Jesus died so that I can become we and so that we can be forgiven no matter what.  We are all invited and desired no matter what.</p>
+
+			<h2>This is it: We are Changed</h2>
+			<p>We may seem weird but what we do together and what we have experienced is crazy awesome and makes us better.  Our devotion is counter-cultural but the miracles we experience remind us that He is with us.  Some of this requires us to look back at what God has done so that he can push us forward together.</p>
+
+			<h2>This is it: We are One</h2>
+			<p>We do everything together.  There is no "I" but rather we choose to live for the other.  But what does that sacrifice mean?  Can you give all of you?  Do you mean it?</p>
+
+			<h2>This is it: We are Committed</h2>
+			<p>Sometimes it may feel repetitive but being the church is a lifestyle that changes the world one day at a time. We must persevere through it even when it’s tough.  Are you in?</p>
+		</div>
+	</div>
+<!--//about-->
+
+	<!--Contact-->
+	<div class="like">		
+		<div class="container">
+			<div class="row">
+				<div class="col-md-8">
+					<h3 class="title1 ">follow us: jesuswalkyouth</h3>
+				<div class="like-info">
+				<div class="col-md-3 like-grids">
+					<a href="https://www.facebook.com/jesuswalkyouth">
+						<img src="images/icon1.png" alt=""/>
+					</a>
+				</div>
+				<div class="col-md-3 like-grids">
+					<a href="https://www.instagram.com/jesuswalkyouth/">
+						<img src="images/icon3.png" alt=""/>
+					</a>
+				</div>
+				<div class="col-md-3 like-grids">
+					<a href="https://twitter.com/jesuswalk">
+						<img src="images/icon5.png" alt=""/>
+					</a>
+				</div>
+				<div class="col-md-3 like-grids">
+					<a href="https://www.youtube.com/channel/UC8ClHBypqvmXQhC8FcMcf_g">
+						<img src="images/icon2.png" alt=""/>
+					</a>
+				</div>			
+				<div class="clearfix"> </div>
+			</div>
+				</div>
+				<div class="col-md-3">
+					<h3 class="title2">contact us</h3>
+				<div class="contact-right">				
+				<form>
+					<input type="text" value="Name" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Name';}" required="">
+					<input type="email" value="Email" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Email';}" required="">
+					<textarea onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Message...';}" required="">Message...</textarea>
+
+				</form>
+			</div>
+				</div>
+			<div class="col-md-1 buttonPosition">
+					<button class="btn" type="submit" ><img src="images/submitButton.png" alt=""/></input>
+			</div>
+
+			</div>
+
+		</div>
+	</div>
+
+	<!--//Contact-->
+
+	<!--footer-->
+<?php
+	include('includes/footer.html');
+	?>
