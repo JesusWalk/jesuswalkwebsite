d9e58f23486fccfbf3f30ce4e12165849b0cb190
mmborado <mmborado@gmail.com>
2016-05-29 02:23:42 -0700

    moved forms to forms folder. Updated get-involved to have button that links to form files. Added responsive structure to the page.

--- get-involved.php
+++ get-involved.php
@@ -14,20 +14,25 @@ include('includes/header.html');
 		</p>
 
 
+			<div class="row">
+				<div class=" col-sm-12 col-md-6" id="volunteer">	
+					<h2>Volunteer Team</h2>
+					<p>
+						There's so much that goes on during the week of JesusWalk, which requires a lot of support to put it all together. In order to make JesusWalk possible, we need volunteers who are willing to serve throughout the entire conference in various roles. You may apply to volunteer if you are over the age of 18. Below is a listing of volunteer descriptions along with a volunteer application for you to fill out. 
 
-		<h2>Volunteer Team</h2>
-		<p>
-			There's so much that goes on during the week of JesusWalk, which requires a lot of support to put it all together. In order to make JesusWalk possible, we need volunteers who are willing to serve throughout the entire conference in various roles. You may apply to volunteer if you are over the age of 18. Below is a listing of volunteer descriptions along with a volunteer application for you to fill out. 
+					</p>
+					<a href="forms/volunteerRegistration.html" target="_blank"><button class="btn btn-info"> Volunteer Registration</button></a>
+				</div>
 
-		</p>
-		<iframe src="https://docs.google.com/forms/d/1luMgq258-Qoh1PcdLwnV5PlydFSwJ5cZczeqO48D1AE/viewform?embedded=true#start=embed" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-
-		<h2>Counselor</h2>
-		<p>
-			Whether it is by taking silly snaps or receiving spiritual guidance, a counselor is a huge part of our conference and what also makes a student’s experience memorable. Plus, who wouldn’t want to spend their summer serving AND having fun?! We are in need for willing individuals to serve alongside us and be an extension of JesusWalk is to our students. You may fill out our interest form below if you fit what we are looking for (please see below). For any other questions or concerns, you may email us at counselorservices@jesuswalkyouth.com. 
-		</p>
-		<iframe src="https://docs.google.com/forms/d/1o_KN_3Y5pskuJosWTEdrehe65K6amqS1H6Vg2Lql6JI/viewform?embedded=true#start=embed" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
-
+				<div class="col-sm-12 col-md-6" id="counselor">
+					<h2>Counselor</h2>
+					<p>
+						Whether it is by taking silly snaps or receiving spiritual guidance, a counselor is a huge part of our conference and what also makes a student’s experience memorable. Plus, who wouldn’t want to spend their summer serving AND having fun?! We are in need for willing individuals to serve alongside us and be an extension of JesusWalk is to our students. You may fill out our interest form below if you fit what we are looking for (please see below). For any other questions or concerns, you may email us at counselorservices@jesuswalkyouth.com. 
+					</p>
+				 	<a href="forms/counselorForm.html" target="_blank"><button class="btn btn-info"> Counselor Registration</button></a>
+					</div>
+				</div>
+			</div>
 	</div>
 </div>
 
