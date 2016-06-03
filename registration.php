b92d10295cac2376f19e49ac4f5bb506f33d1e82
macv101 <michaelvillar0012@gmail.com>
2016-06-02 22:55:52 -0700

    Cleaned up code

--- registration.php
+++ registration.php
@@ -1,250 +1,498 @@
 <?php
+
 	$page_title = "Register";
+
 	$page_banner="registerBanner";
+
 	$page_header="";
+
 	include('includes/header.html');
+
 	?>
 
+
+
 <!--Registration-->
+
 	<div class="registration">
+
 		<div class="container registration">
 
-			<h1>Get Involved</h1>
+
+
+			<h1>Registration is now open!</h1>
+
 			<p>
-				Thank you for your interest in getting more involved with JesusWalk Youth Conference! We truly appreciate your
-				 willingness to help bring amazing things to our students in 2015! See below how you can be more involved with 
-				 JesusWalk 2015: 
+
+				JesusWalk registration is available online. If you're new to our youth conference, we're sure you may 
+
+				have questions. Below, we've provided helpful information to guide you through the process. 
+
 			</p>
+
+
+
+
 
 
 
 			<h2>JW COST</h2>
+
 			<p>
+
 			In the current economy, we know money is tight. That’s why we do everything we can to make the 
+
+
 
 			JesusWalk experience the most affordable for all our students. 
 
+
+
 			We understand it's hard to take your youth group to a youth conference. Especially if you're a small 
+
+
 
 			church and having difficulties finding a budget for attending. We've always said don't let money be the 
 
+
+
 			reason why you can't attend JesusWalk. Which is why we've worked hard to keep costs down, but also to 
 
+
+
 			provide your youth group with the most impactful program to serve you, and further God's Kingdom!
+
 			</p>
 
+
+
 			<div class="highLightInfo">
+
 			<h2>2016 STUDENT COST</h2>
+
 			<p>
+
 			The student cost for JesusWalk covers admission for 4 days; three nights lodging, breakfast, lunch and 
+
+
 
 			dinner for all 4 days. Also included is the welcome pack with all the materials needed for the conference.
 
+
+
 			<ul>
+
+
 
 				<li> <strong>$185* = Individual cost</strong></li>
 
+
+
 				<li> <strong>$180* = For groups of 5 or more</strong></li>
 
+
+
 			</ul>
+
 			</p>
 
+
+
 			<p>
+
 			To register, click on the REGISTER NOW button and follow the instructions.
+
 			</p>
+
+
+
 
 
 			<!-- <a href="https://docs.google.com/forms/d/1j1mqaj5C9pvjUaD3S8coRNwIjiW6ql2KVBTP3KSJauk/viewform?embedded=true" -->
+
 			<a href="/forms/registrationForm.html"
+
 			id="registrationbtn" target="_blank"><button type="button">
+
 			REGISTER NOW</button></a>
 
+
+
 			</div>
 
+
+
 			<br>
+
 			<br>
+
+
 
 			<h2>FOR STUDENTS</h2>		
+
 			<p id="conferenceGoal">
+
 				When you leave this conference, we believe you will have a deeper relationship with God 
+
 				and a clear understanding of how to be a light within your church and community.					
+
 			</p>			
 
+
+
 			<h3>THINGS YOU'LL NEED TO KNOW:</h3>	
+
 			<br>
+
 				<h4>FOOD &amp; LODGING</h4>
+
 				<ul>
+
 					<li>
+
 						Food and lodging IS INCLUDED in the registration. You'll be provided meals for the entire conference. If you have any food allergies, please be sure to list it when you register and/or let our registration team know of your allergies.
+
 					</li>
+
 					<li>
+
 						It is recommended you bring snacks because you will not be allowed to leave the campus at any time during the conference unless it is an emergency.
+
 					</li>
+
 					<li>
+
 						You will need to bring pillows and bed sheets/sleeping bag. These will NOT be provided in the dorm rooms.
+
 					</li>
+
 				</ul>
+
+
 
 				<h4>MONEY</h4>	
+
 				<ul>
+
 					<li>
+
 						You may want to bring money for conference t-shirts, misc items and emergency purposes.
+
 					</li>
+
 				</ul>
+
+
 
 				<h4>IN CASE OF EMERGENCY</h4>
+
 				<ul>
+
 					<li>
+
 						A nurse will be onsite throughout the conference hours. Our nurse is equipped to handle all minor incidents.
+
 					</li>
+
 					<li>
+
 						In case of emergency, if you need to reach your child during the conference, please call 657-234-5992.
+
 					</li>
+
 					<li>
+
 						You should inform our registration team of your child's allergies, medications, or other medical conditions. We would suggest that your youth pastor and chaperones are made aware as well.
+
 					</li>
+
 				</ul>
+
 						
 
+
+
 			<div class="highLightInfo registration">
+
 			<h2>FOR PARENTS</h2>
 
+
+
 				<p>
+
 					JesusWalk Youth Conference is an opportunity for junior high, high school and college students to get away for four days and grow like Jesus did in Luke 2:52 - "And Jesus grew in wisdom and stature, 
+
 					and in favor with God and men." 
+
 					<br/><br/>
 					Our conference is designed to open the eyes of our youth to the message of the Gospel on how to live their life in a material world. We want to show the youth how to live in this world, but not to be of it. With a dynamic program consisting of powerful worship, engaging messages, community service projects and extensive mentoring through small group interaction, young people will leave the conference with a deeper relationship with God and a clear understanding of how to be a light within their church and community.You will need to bring pillows 
+
 					and bed sheets/sleeping bag. These will NOT be provided in the dorm rooms.
+
 				</p>
 
+
+
 				<h3>INFO FOR THE PARENTS</h3>
+
 					<ul>
+
 						<li>
+
 							If your child is under the age of 18, a parental consent form must be signed, which is included in the packet sent out prior to the start of the conference. If you did not receive a form, click here to download a copy.
+
 						</li>
+
 						<li>
+
 							It will be assumed that every child registered for the conference has done so at the consent of the parent, and that all waivers are authentic and signed by the parent.
+
 						</li>
+
 						<li >
+
 							We suggest exchanging the phone numbers of the youth pastors, leaders, and JesusWalk staff contact information prior to their departure to the conference.
+
 						</li>
+
 						<li >
+
 							We provide food and lodging for the entire stay at the conference. For the safety of the attendees and staff, no outside food vendors will be allowed on campus.
+
 						</li>
+
 					</ul>
+
+
 
 				<h3>ENVIRONMENT</h3>
+
 					<ul>
+
 						<li>
+
 							We strive to create a safe, secure and Godly environment for our attendees to experience a life-changing encounter with God.
+
 						</li>
+
 						<li>
+
 							We reserve the right to refuse conference admittance to the conference to any students who are disruptive and propose danger.If this should happen, the costs necessary to transport the student home will be the responsibility of the parent.
+
 						</li>
 						<li>
+
 							We reserve the right to refuse conference admittance to any students who have in their possession drugs, alcohol or weapons.
+
 						</li>
+
 					</ul>
+
+
 
 				<h3>IN CASE OF EMERGENCY</h3>	
+
 					<ul >
+
 						<li>
+
 							A nurse will be onsite throughout the conference hours. Our nurse is equipped to handle all minor incidents.
+
 						</li>
+
 						<li >
+
 							In case of emergency, if you need to reach your child during the conference, please call 657-234-5992.
+
 						</li>
+
 						<li>
+
 							You should inform our registration team of your child's allergies, medications, or other medical conditions. We would suggest that your youth pastor and chaperones are made aware as well.
+
 						</li>
+
 					</ul>
+
 			</div>
 
+
+
 			<div class="registration">
+
 			<h2 >FOR PASTORS</h2>
 
 
+
+
+
 				<p >
+
 					JesusWalk Youth Conference is an opportunity for junior high, high school and college students to get away for four days and grow like Jesus did in Luke 2:52 - "And Jesus grew in wisdom and stature,
+
 					and in favor with God and men." 
+
 					<br/><br/>
+
 					It's designed to open the eyes of our youth to the message of the Gospel on how to live their life in a material world. We want to show the youth how to live in this world, but not to be of it. With a dynamic program consisting of powerful worship, engaging messages, community service projects and extensive mentoring through small group interaction, young people will leave the conference with a deeper relationship with God and a clear understanding of how to be a light within their church and community.
+
 				</p>
 
+
+
 				<h3>JESUSWALK YOUTH CONFERENCE</h3>
+
 				<ul>
+
 					<li>
+
 						JesusWalk is a conference and not a "Youth Camp". Accommodations, food, backpacks, pens, souvenir program are included in the conference registration.
+
 					</li>
+
 					<li>
+
 						Please see the agenda page for an overview of scheduled activities
+
 					</li>
+
 				</ul>
 
+
+
 				<h4>REGISTRATION</h4>		
+
 				<ul>
+
 					<li>
+
 						We strive to create a safe, secure and Godly environment for our attendees to experience a life-changing encounter with God.Registration is available online. Please click on the register link to register for JesusWalk. A downloadable PDF form is also available. Unfortunately, we cannot accept online payments at this time. Please follow the instructions on how to make registration payments.
+
 					</li>
+
 					<li>
+
 						A parental consent form is necessary for each youth attending the conference under the age of 18. Consent forms can be downloaded from the website and manually completed.
+
 					</li>
+
 					<li>
+
 						Students will be the responsibility of the chaperones and youth pastors/leaders of the church they came with.
+
 					</li>
+
 					<li>
+
 						We suggest exchanging the phone numbers of the youth pastors, leaders, and JesusWalk staff contact information prior to their departure to the conference.
+
 					</li>
+
 				</ul>
+
+
+
 
 
 				<h3>FOOD &amp; LODGING</h3>
+
 				<ul>
+
 					<li>
+
 						Food and lodging will be provided and is included in the registration.
+
 					</li>
+
 					<li>
+
 						No outside food vendors will be permitted or allowed on campus.
+
 					</li>
+
 					<li>
+
 						Students may bring snacks with them, but unfortunately will not be able to leave campus during the conference to purchase snacks. Vending machines are located throughout campus where they may purchase drink and snacks.
+
 					</li>
+
 					<li>
+
 						Students will be responsible for their dorm key. Fees will be applied for any lost dorm keys.
+
 					</li>
+
 				</ul>
+
+
+
 
 
 				<h3>ENVIRONMENT</h3>	
+
 				<ul>
+
 					<li>
+
 						We strive to create a safe, secure and Godly environment for our attendees to experience a life-changing encounter with God.
+
 					</li>
+
 					<li>
+
 						We reserve the right to refuse conference admittance to the conference to any students who are disruptive and propose danger.If this should happen, the costs necessary to transport the student home will be the responsibility of the parent.
+
 					</li>
+
 					<li>
+
 						We reserve the right to refuse conference admittance to any students who have in their possession drugs, alcohol or weapons.
+
 					</li>							
+
 				</ul>
 
+
+
 				<h3>IN CASE OF EMERGENCY</h3>
+
 				<ul>
+
 					<li>
+
 						A nurse will be onsite throughout the conference hours. Our nurse is equipped to handle all minor incidents.
+
 					</li>
+
 					<li>
+
 						In case of emergency, if you need to reach your child during the conference, please call 657-234-5992.
+
 					</li>
+
 					<li>
+
 						You should inform our registration team of your child's allergies, medications, or other medical conditions. We would suggest that your youth pastor and chaperones are made aware as well.
+
 					</li>
+
 				</ul>
+
 				</div>
+
 		
+
 		
+
 		</div>	
+
 	</div>
+
+
+
+
+
+
 
 
 
@@ -253,6 +501,11 @@
 <!--//Registration-->
 
 
+
+
+
 <?php
+
 	include('includes/footer.html');
+
 	?>
